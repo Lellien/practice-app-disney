0137e2a27267bfc8cd23a7f8fb2932b19619a289
Lellien <92223291+Lellien@users.noreply.github.com>
2023-07-31 21:48:38 +0100

    improved info formatting and display
    -improved formatting of the character object from api call result.
    - fixed display to show list of additional feature films and list of park attractions.

--- src/disney-api.js
+++ src/disney-api.js
@@ -1,51 +1,90 @@
-function isMaincharacter(name, movie) {
-  let splitName = name.split(" ");
-  return splitName.some((item) => movie.includes(item));
+//display character info
+
+function listParkAttractions(attractionList) {
+  let place = "";
+  attractionList.forEach((item) => {
+    place = place + `<li>${item}</li>`;
+  });
+  return place;
+}
+
+function listMovies(movieList) {
+  let movies = "";
+  movieList.forEach((item, index) => {
+    if (index > 0) {
+      movies = movies + `<li>` + item + `</li>`;
+    }
+  });
+  return movies;
 }
 
 function showCharacter(character) {
-  let mainMovie = character.movies[0];
-  let mainChar = isMaincharacter(character.name, mainMovie);
   let result = document.querySelector("#api-result");
   let display = ` 
   <div class="row mt-4">
   <h2 class="fancy-font" >${character.name}</h2>
-    <div class="col-md-6">
-    <p><strong>${character.name}</strong> is
-     ${
-       mainChar
-         ? `the main protagonist in the movie accordingly named ${mainMovie}.`
-         : `a character from the movie ${mainMovie}`
-     }
-     </br>
-     </br>
-     ${character.name} also appears in the following movies:</p>
-    <ul>
-    ${character.movies.forEach((movie, index) => {
-      if (index > 1) {
-        console.log(movie);
-        return `<li>${movie}</li>`;
-      }
-    })}
-    </ul>
-    </div>
-    <div class="col-md-6">
-   <img class="rounded image-fluid" src="${character.image}" alt="Image of ${
+  <div class="col-md-6">
+  <p><strong>${character.name}</strong> is
+  ${
+    character.leadRole
+      ? `the main protagonist in the movie <em>"${character.mainTitle}"</em>, accordingly named,`
+      : `a character from the movie <em>${character.mainTitle}</em>`
+  }
+ 
+  and also appears in</p>
+  <ul>
+  ${listMovies(character.movies)}
+  </ul>
+<p>${
+    character.name
+  } can be found in real life at Disney park attractions indluding <ul> ${listParkAttractions(
+    character.parkAttractions
+  )}</ul></p>
+<p> To read more about ${character.name}, head over <a href=${
+    character.info
+  } target=_blank> here</a>, a page with tons of information </p>
+  </div>
+  <div class="col-md-6">
+  <img class="rounded image-fluid" src="${character.image}" alt="Image of ${
     character.name
   }"></img>
-    </div>
+  </div>
   </div>`;
   result.classList.remove("hidden");
   result.innerHTML = display;
+}
+
+//format character info
+function isMainCharacter(name, movie) {
+  let splitName = name.split(" ");
+  return splitName.some((item) => movie.includes(item));
+}
+
+function formatMainTitle(movieTitle) {
+  let refinedMovieTitle = movieTitle
+    .split(" ")
+    .filter((segment) => !segment.includes("(") && !segment.includes(")"));
+  let newTitle = "";
+  refinedMovieTitle.forEach((word, index) => {
+    if (index < refinedMovieTitle.length - 1) {
+      newTitle = newTitle + word + " ";
+    } else {
+      newTitle = newTitle + word;
+    }
+  });
+  return newTitle;
 }
 function formatCharacter(characterData) {
   let character = {
     name: characterData.name,
     movies: characterData.films,
+    mainTitle: formatMainTitle(characterData.films[0]),
     parkAttractions: characterData.parkAttractions,
     image: characterData.imageUrl,
     info: characterData.sourceUrl,
   };
+
+  character.leadRole = isMainCharacter(character.name, character.mainTitle);
   showCharacter(character);
 }
 
